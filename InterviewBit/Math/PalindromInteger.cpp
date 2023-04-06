12cb4dfd527e4218211cef42aa86373742dc876a
aman123424 <ch20b010@smail.iitm.ac.in>
2023-04-06 10:49:08 +0530

    Palindrom Integer updated with Method II

--- InterviewBit/Math/PalindromInteger.cpp
+++ InterviewBit/Math/PalindromInteger.cpp
@@ -7,24 +7,45 @@ int isPalindrome(int A) {
     
     if(A < 0) return 0;
     
-    vector<int> ans;
     
-    while(A != 0){
-        int digit = A%10;
-        ans.push_back(digit);
-        A = A/10;    
+    // Method I
+    // vector<int> ans;
+    
+    // while(A != 0){
+    //     int digit = A%10;
+    //     ans.push_back(digit);
+    //     A = A/10;    
+    // }
+    
+    // int s = 0;
+    // int e = ans.size() - 1;
+    
+    // while(s <= e){
+    //     if(ans[s] != ans[e]){
+    //         return 0;
+    //     }
+    //     s++;
+    //     e--;
+    // }
+    
+    // return 1;
+    
+    
+    //Method II
+    
+    int temp = A;
+    int reverse = 0;
+    
+    while(temp != 0){
+        int digit = temp%10;
+        
+        reverse = reverse*10 + digit;
+        temp /= 10;
     }
     
-    int s = 0;
-    int e = ans.size() - 1;
-    
-    while(s <= e){
-        if(ans[s] != ans[e]){
-            return 0;
-        }
-        s++;
-        e--;
+    if(A == reverse){
+        return 1;
     }
     
-    return 1;
+    return 0;
 }
